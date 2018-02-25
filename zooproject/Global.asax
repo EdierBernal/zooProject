664ee10a19591956315920bf6b577d81376fe757
Edier Bernal <edier.bernal@outlook.com>
2018-02-24 22:55:00 -0500

    Add project files.

---
+++ zooproject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zooproject.MvcApplication" Language="C#" %>
---
+++ zooproject/Scripts/jsGeneral.js
@@ -0,0 +1,4 @@
+﻿function openNewTabBrowser() {
+  //alert("Success");
+  window.open('https://itceduco-my.sharepoint.com/:f:/g/personal/ejbernalr_itc_edu_co/El4MseapVptJhpjv5ZtDlNsBhNfp_jU-lGU4fpTk9bpPoQ?e=mdCec8')
+}
