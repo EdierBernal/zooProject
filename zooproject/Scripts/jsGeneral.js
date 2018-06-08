c0a316f6ce1e8877c8cf20c67dacbd6e0e86d5a9
Edier Bernal <edier.bernal@bitsamericas.com>
2018-06-08 17:29:56 -0500

    Cambios Ultimo corte. Agregan interfaces y mejoras en página.

--- zooproject/Scripts/jsGeneral.js
+++ zooproject/Scripts/jsGeneral.js
@@ -10,3 +10,8 @@ function openNewTabBrowser_Metodologia() {
   //alert("Success");
   window.open('https://itceduco-my.sharepoint.com/:w:/g/personal/ejbernalr_itc_edu_co/EYamwCKD2iZNtzcjVyYERHYB0dxctBNsiYDyFfkMplAryQ?e=ifIOMZ')
 }
+
+function openNewTabBrowser_Interfaces() {
+  //alert("Success");
+  window.open('https://itceduco-my.sharepoint.com/:w:/r/personal/ejbernalr_itc_edu_co/_layouts/15/Doc.aspx?sourcedoc=%7B8EE65BD7-9D60-4C63-8B6A-9C7F74BF7703%7D&file=Dise%C3%B1o%20de%20interfaces.docx&action=default&mobileredirect=true')
+}
